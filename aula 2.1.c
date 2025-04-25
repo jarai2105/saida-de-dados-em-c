601f7983933833dfd21cc4f55f41aebe63ae675d
Jaraí Santos <jaraisantos88@gmail.com>
2025-04-24 22:04:29 -0300

    Add files via upload

---
+++ aula 2.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("Oi, tenho 36 ânus e programo.\n");
+	printf("Valor inteiro:%d.\n", 11);
+	printf("Valor real: %f.\n", 3.14159265);
+	printf("Valor real com duas casas: %.2f.\n", 3.14159265);
+	printf("Dado de texto: %c.\n",'a');
+	printf("Dado de texto: %s.\n","Teste");
+	return 0;
+}
